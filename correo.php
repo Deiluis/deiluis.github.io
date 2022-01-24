17637da323dc8f3d99809d1d05f3e3f05e9718e9
Deiluis <luisconforti04@gmail.com>
2022-01-24 18:11:42 -0300

    1ra prueba de enviar mail con formulario

---
+++ correo.php
@@ -0,0 +1,13 @@
+<?php
+    $destinatario = 'luisconforti04@gmail.com';
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $asunto = $_POST['asunto'];
+    $telefono = $_POST['telefono'];
+    $mensaje = $_POST['nombre'];
+
+    $header = "Consulta desde la página web del ISFD";
+    $mensajeCompleto = "Consulta de: " . $nombre . "\nEmail: " . $email . "\nTelefóno: " . $telefono . "\n\nMensaje: " . $mensaje;
+
+    mail($destinatario, $asunto, $mensajeCompleto, $header);
+?>
