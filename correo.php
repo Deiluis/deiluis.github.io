d4c683afdfbeb1b3c758c7e53f81103a516dfd85
Deiluis <luisconforti04@gmail.com>
2022-01-24 20:20:09 -0300

    Cambio

--- correo.php
+++ correo.php
@@ -1,18 +1,13 @@
 <?php
-
-    if(isset($_POST['enviar'])){
-
         $destinatario = 'luisconforti04@gmail.com';
         $nombre = $_POST['nombre'];
         $email = $_POST['email'];
         $asunto = $_POST['asunto'];
         $telefono = $_POST['telefono'];
-        $mensaje = $_POST['nombre'];
+        $mensaje = $_POST['mensaje'];
 
         $header = "Consulta desde la página web del ISFD";
         $mensajeCompleto = "Consulta de: " . $nombre . "\nEmail: " . $email . "\nTelefóno: " . $telefono . "\n\nMensaje: " . $mensaje;
 
-        mail($destinatario, $asunto, $mensajeCompleto, $header);
-
-    }
+        mail($destinatario, $asunto, $mensajeCompleto);
 ?>
